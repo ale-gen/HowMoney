a668986a9de00f2ba6317ff1276d72524aa36f60
Aleksandra Generowicz <o.generowicz@gmail.com>
2022-10-08 16:38:21 +0200

    Init workspace

---
+++ HowMoney/HowMoney/HowMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMoneyApp.swift
+//  HowMoney
+//
+//  Created by Aleksandra Generowicz on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HowMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
