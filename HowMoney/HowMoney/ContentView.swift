b9517b4e78645d0f36af2f0f4b70fd3e3858675d
Aleksandra Generowicz <o.generowicz@gmail.com>
2022-10-25 16:00:49 +0200

    HOWM-89
    Fix mode colors when user have light mode turned on

--- HowMoney/HowMoney/ContentView.swift
+++ HowMoney/HowMoney/ContentView.swift
@@ -10,8 +10,15 @@ import Auth0
 
 struct ContentView: View {
     
+    private enum Constants {
+        static let topPadding: CGFloat = 1.0
+        static let preferredColorScheme: ColorScheme = .dark
+    }
+    
     var body: some View {
         AppSwitcher(authService: AuthorizationService())
+            .padding(.top, Constants.topPadding)
+            .preferredColorScheme(Constants.preferredColorScheme)
     }
 }
 
