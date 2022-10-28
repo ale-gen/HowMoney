482e7df57b195f427b8bd0f2b9b5797143beaa4a
Aleksandra Generowicz <o.generowicz@gmail.com>
2022-10-28 18:38:24 +0200

    HOWM-88
    Code refactor

--- HowMoney/HowMoney/Presentation/TabBar/TabBarView.swift
+++ HowMoney/HowMoney/Presentation/TabBar/TabBarView.swift
@@ -29,48 +29,6 @@ struct TabBarView: View {
     }
 }
 
-class TabBarContentViewRouter: ObservableObject {
-    
-    enum TabBarContent {
-        case home
-        case wallet
-        case transactions
-        case profile
-        
-        var contentView: AnyView {
-            switch self {
-            case .home:
-                return AnyView(HomeTabBarItem())
-            case .wallet:
-                return AnyView(UserAssetsTabBarItem(searchText: .constant(.empty)))
-            case .transactions:
-                return AnyView(TransactionsTabBarItem())
-            case .profile:
-                return AnyView(ProfileTabBarItem())
-            }
-        }
-    }
-     
-    @Published var currentContent: AnyView = TabBarContent.home.contentView
-    
-    func navigateToContent(_ selectedTabBar: TabBarItem) {
-        guard selectedTabBar != .plus else { return }
-        switch selectedTabBar {
-        case .home:
-            currentContent = TabBarContent.home.contentView
-        case .wallet:
-            currentContent = TabBarContent.wallet.contentView
-        case .transactions:
-            currentContent = TabBarContent.transactions.contentView
-        case .profile:
-            currentContent = TabBarContent.profile.contentView
-        default:
-            break
-        }
-    }
-    
- }
-
 struct TabBarView_Previews: PreviewProvider {
     static var previews: some View {
         TabBarView()
